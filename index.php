d67fe8792f7e6a5af669c5b53c31666ce07d6ed9
macmuriithi <50452802+macmuriithi@users.noreply.github.com>
2023-09-16 11:31:40 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,20 @@
+<?php
+//Include Environment,SOP,State,LLM,Semantic Search,Tool,Agent
+$environment = new Environment(/* provide components */);
+$sop = new SOP();
+$llm = new LLM(); // Initialize the Language Model Manager
+$semanticSearch = new SemanticSearch(); // Initialize Semantic Search
 
+$llmTool = new Tool("LLMTool");
+$semanticSearchTool = new Tool("SemanticSearchTool");
+
+$agent1 = new Agent([$llmTool], false, $semanticSearch, $llm); // Agent 1 with LLMTool and Semantic Search
+$agent2 = new Agent([$semanticSearchTool], false, $semanticSearch, $llm); // Agent 2 with SemanticSearchTool and Semantic Search
+$agent3 = new Agent([], true, $semanticSearch, $llm); // Agent 3 as a human with Semantic Search
+
+$sop->addState(new State("State 1"));
+$sop->addState(new State("State 2"));
+
+$moderator = new Moderator($sop, [$agent1, $agent2, $agent3], $environment);
+
+$moderator->runSimulation();
