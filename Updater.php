5bcd717f7533f3dc506674cdbb435c15a7cee989
macmuriithi <50452802+macmuriithi@users.noreply.github.com>
2023-09-16 11:57:48 +0300

    Update Updater.php

--- Updater.php
+++ Updater.php
@@ -1,6 +1,6 @@
 <?php
 class Updater {
-    public function updateMemory(array $agents, $action) {
+    public function updateMemories(array $agents, $action) {
         // Implement logic to update the memory of each agent based on the action
         foreach ($agents as $agent) {
             $agent->updateMemory($action);
